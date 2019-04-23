505ae56c0a988df6eff7b09789a949766bac7570
zhxt95 <zhxt1995@163.com>
2019-04-23 09:42:58 +0000

    mysql db-type with env config for pymsql

--- mlflow/store/dbmodels/db_types.py
+++ mlflow/store/dbmodels/db_types.py
@@ -1,9 +1,9 @@
 """
 Set of SQLAlchemy database schemas supported in MLflow for tracking server backends.
 """
-
+import os
 POSTGRES = 'postgresql'
-MYSQL = 'mysql'
+MYSQL = os.environ.get("MYSQLDB",'mysql+pymysql')   #os.environ['MYSQLDB']  #'mysql'
 SQLITE = 'sqlite'
 MSSQL = 'mssql'
 
